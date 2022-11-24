8ea2a60f3041138ec5eb5b54922c72f83a2503c0
ifazzzz <shoaibahmedifaz@gmail.com>
2022-11-24 13:22:01 +0600

    categories made

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wbjzicb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const categoriesCollection = client.db('readers-port').collection('categories');
+
+app.get('/categories', async (req, res) =>{
+    const query = {}
+    const categories = await categoriesCollection.find(query).toArray();
+    res.send(categories);
+})
+
+
+app.get('/', (req, res) => {
+    res.send('server running')
+})
+
+app.listen(port, ()=> {
+    console.log(`listening on port ${port}`);
+})
